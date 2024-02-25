723e270e4247ba35a3ec509785ed5a4080252161
Dilnoza-frontend <dilnoza_matyakubova@yahoo.com>
2024-02-24 23:21:14 -0500

    Add navigator clipboard write text

--- js/main.js
+++ js/main.js
@@ -19,8 +19,12 @@ if (evt.target.matches('.js-copy-short-link-button')) {
    // Change button bgcolor
    evt.target.classList.add('url-shortener__copy-button--copied');
 
-// Reset button text and class after 1 second
-setTimeout(function(){
+  //Copy short link to clipboard
+  navigator.clipboard.writeText(evt.target.previousElementSibling.textContent);
+
+
+  // Reset button text and class after 1 second
+  setTimeout(function(){
   evt.target.textContent = 'Copy';
   evt.target.classList.remove('url-shortener__copy-button--copied');
 }, 1000);
